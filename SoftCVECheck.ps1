f91bec4cc7de95d1d0302f415561f97600833579
PingBackHome <115549820+PingBackHome@users.noreply.github.com>
2023-03-20 14:49:35 +0100

    Update SoftCVECheck.ps1

--- SoftCVECheck.ps1
+++ SoftCVECheck.ps1
@@ -1,49 +1,39 @@
-# Define variables
-$csvPath = "$env:USERPROFILE\Desktop\InstalledSoftware.csv"
-$reportPath = "$env:USERPROFILE\Desktop\SecurityReport.txt"
-$cveUrl = "https://cve.mitre.org/cgi-bin/cvekey.cgi?keyword="
-$results = @()
+# Get current user's desktop path
+$desktop_path = [Environment]::GetFolderPath("Desktop")
 
-# Get installed software
-$software = Get-ItemProperty HKLM:\Software\Wow6432Node\Microsoft\Windows\CurrentVersion\Uninstall\* | 
-            Select-Object DisplayName, DisplayVersion | 
-            Sort-Object DisplayName
+# Get list of installed software with versions using 'Get-ItemProperty'
+$software_list = Get-ItemProperty HKLM:\Software\Wow6432Node\Microsoft\Windows\CurrentVersion\Uninstall\* |
+    Select-Object DisplayName, DisplayVersion |
+    Sort-Object DisplayName
 
-# Loop through software and check for CVEs
-foreach ($app in $software) {
-    $cve = $null
-    if ($app.DisplayVersion) {
-        $cveUrl = $cveUrl + "$($app.DisplayName) $app.DisplayVersion"
-    } else {
-        $cveUrl = $cveUrl + "$($app.DisplayName)"
-    }
-    $cvePage = Invoke-WebRequest -Uri $cveUrl -ErrorAction SilentlyContinue
-    if ($cvePage.StatusCode -eq 200) {
-        $cve = $cvePage.ParsedHtml.getElementsByTagName("a") | 
-               Where-Object {$_.innerText -match "^CVE-\d{4}-\d+$"} | 
-               Select-Object -First 1 -ExpandProperty innerText
-    }
-    $results += [PSCustomObject]@{
-        "Software" = $app.DisplayName
-        "Version" = $app.DisplayVersion
-        "CVE" = $cve
+# Write software list to CSV file
+$csv_path = Join-Path $desktop_path "installed_software.csv"
+$software_list | Export-Csv -Path $csv_path -NoTypeInformation
+
+# Compare software versions with NVD CVE database
+$cve_report = ""
+foreach ($software in $software_list) {
+    $name = $software.DisplayName
+    $version = $software.DisplayVersion
+
+    # Build search URL
+    $search_url = "https://nvd.nist.gov/vuln/search/results?form_type=Advanced&results_type=overview&search_type=all&cpe_vendor=cpe%3A%2F%3A$name&cpe_product=cpe%3A%2F%3A$name%3A$version"
+
+    # Send search request
+    $response = Invoke-WebRequest -Uri $search_url
+
+    # Parse response for vulnerabilities
+    $vuln_list = $response.ParsedHtml.GetElementsByClassName("vuln-title")
+
+    # Add vulnerabilities to report
+    if ($vuln_list.Count -gt 0) {
+        $cve_report += "`n`nVulnerabilities for $name version $version:`n`n"
+        foreach ($vuln in $vuln_list) {
+            $cve_report += "- $($vuln.InnerText)`n"
+        }
     }
 }
 
-# Export results to CSV
-$results | Export-Csv -Path $csvPath -NoTypeInformation -Encoding UTF8
-
-# Generate report
-$report = "Security Report`r`n`r`n"
-$report += "Installed Software:`r`n"
-$report += "===================`r`n`r`n"
-$results | Where-Object {$_.CVE} | 
-    ForEach-Object {
-        $report += "$($_.Software) $($_.Version) has a known CVE: $($_.CVE)`r`n"
-    }
-if ($report -eq "Security Report`r`n`r`nInstalled Software:`r`n===================`r`n`r`n") {
-    $report += "No known vulnerabilities found.`r`n"
-}
-
-# Save report to file
-Set-Content -Path $report
+# Write vulnerability report to text file
+$txt_path = Join-Path $desktop_path "vulnerability_report.txt"
+Set-Content -Path $txt_path -Value $cve_report
